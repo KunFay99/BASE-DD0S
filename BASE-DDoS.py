05463f3bd6427bb5909eec8dc322ecf5276fc2e2
KunFay99 <kaffayukiffu@gmail.com>
2024-12-20 01:17:55 +0700

    Update BASE-DDoS.py

--- BASE-DDoS.py
+++ BASE-DDoS.py
@@ -72,7 +72,7 @@ def start_ddos(prox, url, headers, proxies, color):
 		s = r.Session()
 		req = s.get(url, headers=headers, proxies=proxies)
 		if req.status_code == 200:
-			print(color+"{}::..BASE ".format(prox))
+			print(color+"{}_proxy ".format(prox))
 	except:
 		pass
 
